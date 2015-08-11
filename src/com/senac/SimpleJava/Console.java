91e85477bd3af8c5e68b66cbc27f041397379073
Rafael Jeffman <rafael.jeffman@gmail.com>
2015-08-11 10:56:49 -0300

    Added options to read from the standard input without a prompt.

--- src/com/senac/SimpleJava/Console.java
+++ src/com/senac/SimpleJava/Console.java
@@ -15,7 +15,7 @@ public final class Console {
 	 * @param arguments The list of arguments.
 	 */
 	public static void print(Object... arguments) {
-		StringBuilder sb = new StringBuilder ();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < arguments.length; i++)
 			sb.append (arguments[i].toString());
 		System.out.print(sb);
@@ -27,20 +27,36 @@ public final class Console {
 	 * @param arguments The list of arguments.
 	 */
 	public static void println(Object... arguments) {
-		StringBuilder sb = new StringBuilder ();
+		StringBuilder sb = new StringBuilder();
 	    for (int i = 0; i < arguments.length; i++)
 	    		sb.append (arguments[i].toString());
 	    System.out.println(sb);
 	}
-	
+
+	/**
+	 * Prints a newline character on the standard output.
+	 */
+	public static void println() {
+		System.out.println();
+	}
+
 	/**
 	 * Prompts for  a line of text from the standard input and returns
 	 * it as a single string.
-	 * @param prompt The prompt to show.
 	 * @return The text from the line read as input.
 	 */
 	public static String readLine(String prompt) {
 		System.out.print(prompt);
+		keyboard.skip("[\r\n]*");
+		return keyboard.nextLine();
+	}
+	
+	/**
+	 * Reads a line of text from the standard input and returns
+	 * it as a single string.
+	 * @return The text from the line read as input.
+	 */
+	public static String readLine() {
 		keyboard.skip("[\r\n]*");
 		return keyboard.nextLine();
 	}
@@ -58,6 +74,16 @@ public final class Console {
 	}
 	
 	/**
+	 * Reads a string from the standard input and returns it.
+	 * The string returned will be limited by any of TAB, space, new
+	 * line or carriage return characters.
+	 * @return The string read as input.
+	 */
+	public static String readString() {
+		return keyboard.next();
+	}
+	
+	/**
 	 * Prompts for an integer number from the standard input.
 	 * @param prompt The prompt to show.
 	 * @return The integer read as input.
@@ -66,7 +92,15 @@ public final class Console {
 		System.out.print(prompt);
 		return keyboard.nextInt();
 	}
-	
+
+	/**
+	 * Reads an integer number from the standard input.
+	 * @return The integer read as input.
+	 */
+	public static int readInt() {
+		return keyboard.nextInt();
+	}
+
 	/**
 	 * Prompts for a real number from the standard input and returns it.
 	 * @param prompt The prompt to show.
@@ -74,6 +108,14 @@ public final class Console {
 	 */
 	public static double readDouble(String prompt) {
 		System.out.print(prompt);
+		return keyboard.nextDouble();
+	}
+
+	/**
+	 * Reads a real number from the standard input and returns it.
+	 * @return The double read as input.
+	 */
+	public static double readDouble() {
 		return keyboard.nextDouble();
 	}
 }
