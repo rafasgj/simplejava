dd74f186825f1c51f14e295a25c34df7d0caad3b
Rafael Jeffman <rafael.jeffman@gmail.com>
2015-08-20 10:31:21 -0300

    Fixed markdwon output directory.

--- scripts/html2md.sh
+++ scripts/html2md.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# This does not really work well with much more work into it.
+# I'd be very pleased if someone takes the task to convert the
+# class reference javadocs (HTML) to Markdown so that it can be
+# the project's Wiki. -- Rafael
+#
+
 if [ $# -ne 2 ]
 then
     echo "usage: `basename $0` <javadoc dir> <markdown dir>" >&2
@@ -14,9 +21,10 @@ FILES="`find "${SRC}/" -name '*.html'`"
 
 for f in $FILES
 do
-    echo "Converting `echo "$f" | sed 's#//*#/#g'`."
-    dir="${TARGET}/`dirname "$f"`"
+    f="`echo "$f" | sed 's#//*#/#g' | cut -d/ -f2-`"
+    echo "Converting ${f}"
+    dir="${TARGET}/`dirname "${f}"`"
     mkdir -p "${dir}/"
-    pandoc -f html -t markdown -o "${dir}/`basename "$f" .html`.md" -s "$f"
+    pandoc -f html -t markdown -o "$dir/`basename "$f" .html`.md" -s "$SRC/$f"
 done
 
